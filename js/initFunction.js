ad286652e5fdd41f31d2b6797b61bacfeca18370
Anger romain <anger.romain@gmail.com>
2021-10-23 19:21:49 +0200

    aucune avancer particuliere, demontage et remontage de fonction pour trouver le bug

--- js/initFunction.js
+++ js/initFunction.js
@@ -1,13 +1,13 @@
 // fonction du random
 const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // fonction du choix de la liste
 const displayChoseThemes = () => {
-    els.themes.innerHTML = `<h2 class="d-flex justify-content-center col-lg-12">Choisissez votre themes</h2>
+  els.themes.innerHTML = `<h2 class="d-flex justify-content-center col-lg-12">Choisissez votre themes</h2>
                         <div class="col-lg-12">
                             <ul class="d-flex flex-wrap justify-content-center align-content-center col-lg-12">
                                 <li id="tous" class="d-flex col-8 justify-content-center">Tous les mots</li>
@@ -17,100 +17,119 @@ const displayChoseThemes = () => {
                         </div>`;
 };
 
+// ecoute du choix du theme
+const choiceTheme = () => {
+    els.themes.addEventListener('click', ({target}) => {
+        if (target.matches('#tous')) {
+        themeChosen = wordsListTotal;
+        return themeChosen;
+        } else if (target.matches('#animaux')) {
+        themeChosen = wordsListAnimaux;
+        return themeChosen;
+        } else if (target.matches('#prenom')) {
+        themeChosen = wordsListPrenom;
+        return themeChosen;
+        }
+        console.log(themeChosen);
+    });
+    
+    // return themeChosen;
+}
+
 //Conversion des tableaus en majuscule
 const lowerToUpper = () => {
-    for (let word of listChosen){
-        let toUpper = word.toLocaleUpperCase();
-        words.push(toUpper);       
-    }
-    return words;
+  for (let word of themeChosen) {
+    let toUpper = word.toLocaleUpperCase();
+    words.push(toUpper);
+  }
+  return words;
 };
 
 // fonction qui permet de choisir un mot de la liste au hazar
 const pickWord = () => {
-    const randomIndex = getRandomInt(0, words.length - 1);
-    console.log(word);
-    return words[randomIndex];
+  const randomIndex = getRandomInt(0, words.length - 1);
+  return words[randomIndex];
 };
 
 // fonction pour la création du tableau de lettre du mot rechercher
 const getWordMapping = (word) => {
-    const wordArr = word.split('');
-    const wordMapping = wordArr.map((letter, index) => {
-        let isVisible = false;
-        if (index === 0 || index == wordArr.length - 1) {
-            isVisible = true;
-        }
-        return {
-            letter, 
-            isVisible
-        };
-    });
-    return wordMapping;
+  const wordArr = word.split('');
+  console.log(wordArr);
+  const wordMapping = wordArr.map((letter, index) => {
+    let isVisible = false;
+    if (index === 0 || index == wordArr.length - 1) {
+      isVisible = true;
+    }
+    return {
+      letter,
+      isVisible
+    };
+  });
+  return wordMapping;
 };
 
 // fonction de génération du clavier de lettre
 const generateChoices = () => {
-    const choices = [];
-    for(let index = 65; index <= 90; index++) {
-        choices.push(String.fromCharCode(index));
-    }
-    return choices;
+  const choices = [];
+  for (let index = 65; index <= 90; index++) {
+    choices.push(String.fromCharCode(index));
+  }
+  return choices;
 };
 
 // fonction qui recupere la lettre choisi par l'utilisateur
 const getChoicesMapping = (choices) => {
-    const choicesMapping = choices.map((letter) => {
-        return {
-            letter,
-            isChosen: false
-        };
-    });
-    return choicesMapping;
+  const choicesMapping = choices.map((letter) => {
+    return {
+      letter,
+      isChosen: false
+    };
+  });
+  return choicesMapping;
 };
 
 //fonction de l'affichage de la lettre par l'utilisateur
 const displayChoices = (choicesMapping) => {
-    const choicesHtml = choicesMapping.map((letterMapping) => {
-        if (letterMapping.isChosen === false) {
-            return `<li>${letterMapping.letter}</li>`;
-        } else {
-            return `<li class="disabled">${letterMapping.letter}</li>`;
-        }
-    });
-    els.choices.querySelector('ul').innerHTML = choicesHtml.join('');
+  const choicesHtml = choicesMapping.map((letterMapping) => {
+    if (letterMapping.isChosen === false) {
+      return `<li>${letterMapping.letter}</li>`;
+    } else {
+      return `<li class="disabled">${letterMapping.letter}</li>`;
+    }
+  });
+  els.choices.querySelector('ul').innerHTML = choicesHtml.join('');
 };
 
 // fonction d'affichage du score restant avant la mort
 const displayScore = () => {
-    // els.score.innerHTML = `${scoreCount} / ${maxScore}`;  <== version en donnée brut 
-    els.score.innerHTML = `<img src="img/pendu/0${scoreCount}.png" alt="hangman" />`; // version avec image du pendu
+  // els.score.innerHTML = `${scoreCount} / ${maxScore}`;  <== version en donnée brut 
+  els.score.innerHTML = `<img src="img/pendu/0${scoreCount}.png" alt="hangman" />`; // version avec image du pendu
 };
 
 // fonction de remplacement des lettres par des underscores
 const displayWord = (wordMapping) => {
-    const wordHtml = wordMapping.map((letterMapping) => {
-        if (letterMapping.isVisible === true) {
-            return `<li>${letterMapping.letter}</li>`;
-        } else {
-            return `<li>_</li>`;
-        }
-    });
+  const wordHtml = wordMapping.map((letterMapping) => {
+    if (letterMapping.isVisible === true) {
+      return `<li>${letterMapping.letter}</li>`;
+    } else {
+      return `<li>_</li>`;
+    }
+  });
 
-    els.answer.querySelector('ul').innerHTML = wordHtml.join('');
+  els.answer.querySelector('ul').innerHTML = wordHtml.join('');
 };
 
 // fonction d'affichage de fin de partie
 
 // Si le score == maxscore: "tu est mort"
 const endGame = () => {
-    wordMapping.forEach(w => w.isVisible = true);
-    displayWord(wordMapping);
-    // document.querySelector('body').style.backgroundColor = 'red';
-    els.choices.innerHTML = `<h1 class="d-flex justify-content-center col-lg-12 ft-color-sec">Vous êtes mort !</h1>`;
-  };
-  
-  // si le mot est trouvé: "tu est libre"
-  const winGame = () => {
-    els.choices.innerHTML = `<h1 class="d-flex justify-content-center col-lg-12 ft-color-sec">Courez!! vous êtes libre</h1>`;
-  }
+  wordMapping.forEach(w => w.isVisible = true);
+  displayWord(wordMapping);
+  // document.querySelector('body').style.backgroundColor = 'red';
+  els.choices.innerHTML = `<h1 class="d-flex justify-content-center col-lg-12 ft-color-sec">Vous êtes mort !</h1>`;
+};
+
+// si le mot est trouvé: "tu est libre"
+const winGame = () => {
+  els.choices.innerHTML = `<h1 class="d-flex justify-content-center col-lg-12 ft-color-sec">Courez!! vous êtes libre</h1>`;
+}
--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ let choicesMapping = [];
 let scoreCount = 0;
 let maxScore = 8;
 let words = [];
-let listChosen = [];
+let themeChosen = [];
 
 //initialisation et execution du jeu 
 const init = () => {
@@ -21,114 +21,97 @@ const init = () => {
   // définition des elements html
   els.score = document.querySelector('#score');
   els.answer = document.querySelector('#answer');
-  els.choices = document.querySelector('#choices');
   els.themes = document.querySelector('#themes');
+  els.choices = document.querySelector('#choices');
 
-  // affichage des choix de liste de mot
+  // affichage du choix des themes
   displayChoseThemes();
-  // choix de la liste de mot
-  els.themes.addEventListener('click', ({
-    target
-  }) => {
-    if (target.matches('#tous')) {
-      listChosen = wordsListTotal;
-      //   console.log(listChosen);
-      return listChosen;
-    } else if (target.matches('#animaux')) {
-      listChosen = wordsListAnimaux;
-      //   console.log(listChosen);
-      return listChosen;
-    } else if (target.matches('#prenom')) {
-      listChosen = wordsListPrenom;
-      //   console.log(listChosen);
-      return listChosen;
-    }
-  });
-  console.log(listChosen);
-  // Transforme tout les mot en majuscule
-  lowerToUpper();
-  // choix du mot
-  console.log(words);
-  word = pickWord();
-  console.log(word);
-  // création du mot en liste de lettre
-  wordMapping = getWordMapping(word);
-  // Generation du choix de lettre
-  choices = generateChoices();
-  // recupération du choix de la lettre
-  choicesMapping = getChoicesMapping(choices);
-  // affichage du mot
-  displayWord(wordMapping);
-  // affichage du clavier de lettre
-  displayChoices(choicesMapping);
-  // Display score
-  // displayScore();
-
-  // Listen events
-  //    - mouse events
-  els.choices.addEventListener('click', ({
-    target
-  }) => {
-    if (target.matches('li')) {
-      checkLetter(target.innerHTML);
-    }
-  });
-
-  //    - keyboard events
-  document.addEventListener('keydown', ({
-    keyCode
-  }) => {
-    const letter = String.fromCharCode(keyCode);
-    if (keyCode >= 65 && keyCode <= 90) {
-      checkLetter(letter);
-    }
-  });
+// };
+  // capture du choix du theme
+  choiceTheme();
+  
 };
+// words = lowerToUpper(themeChosen);
+//   console.log("animaux : ", wordsListAnimaux.length);
+//   console.log("prenom : ", wordsListPrenom.length);
+//   console.log("total : ", wordsListTotal.length);
+//   console.log("theme : ", themeChosen.length);
+//   console.log("words : ", words);
 
-// verification de la lettre
-const checkLetter = (letter) => {
+//   word = pickWord(words);
+//   // création du mot en liste de lettre
+//   wordMapping = getWordMapping(word);
+//   // Generation du choix de lettre
+//   choices = generateChoices();
+//   // recupération du choix de la lettre
+//   choicesMapping = getChoicesMapping(choices);
+//   // affichage du mot
+//   displayWord(wordMapping);
+//   // affichage du clavier de lettre
+//   displayChoices(choicesMapping);
+//   // Display score
+//   // displayScore();
 
-  let isLetterInWord = false;
-  let isAllLettersFound = true;
+//   // Listen events
+//   //    - mouse events
+//   els.choices.addEventListener('click', ({
+//     target
+//   }) => {
+//     if (target.matches('li')) {
+//       checkLetter(target.innerHTML);
+//     }
+//   });
 
-  //  - si elle est dans le mot: afficher la letre
-  wordMapping.forEach((letterMapping) => {
-    if (letterMapping.letter === letter) {
-      letterMapping.isVisible = true;
-      isLetterInWord = true;
-    }
-    //  - si elle n'est pas dans le mot:
-    if (!letterMapping.isVisible) {
-      isAllLettersFound = false;
-    }
-  });
-
-  choicesMapping.forEach((letterMapping) => {
-    if (letterMapping.letter === letter) {
-      letterMapping.isChosen = true;
-    }
-  });
-
-  displayChoices(choicesMapping);
-  if (isLetterInWord === true) {
-    displayWord(wordMapping);
-  } else {
-    scoreCount++;
-    displayScore();
-  }
-
-  if (scoreCount === maxScore) {
-    endGame();
-  }
-  if (isAllLettersFound) {
-    winGame();
-  }
-};
-
-
+//   //    - keyboard events
+//   document.addEventListener('keydown', ({
+//     keyCode
+//   }) => {
+//     const letter = String.fromCharCode(keyCode);
+//     if (keyCode >= 65 && keyCode <= 90) {
+//       checkLetter(letter);
+//     }
+//   });
 
 
+// // verification de la lettre
+// const checkLetter = (letter) => {
 
+//   let isLetterInWord = false;
+//   let isAllLettersFound = true;
+
+//   //  - si elle est dans le mot: afficher la letre
+//   wordMapping.forEach((letterMapping) => {
+//     if (letterMapping.letter === letter) {
+//       letterMapping.isVisible = true;
+//       isLetterInWord = true;
+//     }
+//     //  - si elle n'est pas dans le mot:
+//     if (!letterMapping.isVisible) {
+//       isAllLettersFound = false;
+//     }
+//   });
+
+//   choicesMapping.forEach((letterMapping) => {
+//     if (letterMapping.letter === letter) {
+//       letterMapping.isChosen = true;
+//     }
+//   });
+
+//   displayChoices(choicesMapping);
+//   if (isLetterInWord === true) {
+//     displayWord(wordMapping);
+//   } else {
+//     scoreCount++;
+//     displayScore();
+//   }
+
+//   if (scoreCount === maxScore) {
+//     endGame();
+//   }
+//   if (isAllLettersFound) {
+//     winGame();
+//   }
+// };
 
 window.addEventListener('load', () => {
   init();
